50bfdcd7241554aa986fdb14d9f99e853871e60d
brahmaredy185 <brahma@xelware.com>
2025-10-04 14:30:17 +0530

    create header section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { Button, Navbar, NavbarCollapse, NavbarLink, NavbarToggle, TextInput } from "flowbite-react"
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+function Header() {
+    const pathname = useLocation().pathname;
+  return (
+    <Navbar className="border-b-1 p-4" fluid rounded>
+        <Link to='/' className="self-center flex justify-between items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+            <span 
+                className="px-2 py-1 bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500 rounded-lg text-white"
+            >
+                Red's
+            </span> Blog
+        </Link>
+        <form>
+            <TextInput 
+                type="text"
+                placeholder="Search..."
+                con={AiOutlineSearch}
+                className="hidden lg:inline"
+            />
+        </form>
+        <Button className="w-12 h-10" color="gray"><AiOutlineSearch /></Button>
+        <div className='flex gap-2 md:order-2'>
+            <Button className="bg-gradient-to-r from-blue-500 px-4 py-2 via-blue-600 to-blue-700 text-white hover:bg-gradient-to-br focus:ring-blue-300 dark:focus:ring-blue-800">
+    <FaMoon /></Button>
+            <Link to='/signin'>
+                <Button color="blue" outline>
+                    Sign In
+                </Button>
+          </Link>
+          
+        </div>
+        <NavbarToggle />
+        <NavbarCollapse>
+        <NavbarLink as={Link} to="/" active={pathname === "/"}>
+          Home
+        </NavbarLink>
+        <NavbarLink as={Link} to="/about" active={pathname === "/about"}>
+          About
+        </NavbarLink>
+        <NavbarLink as={Link} to="/projects" active={pathname === "/projects"}>
+          Projects
+        </NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  )
+}
+
+export default Header
