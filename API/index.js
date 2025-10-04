7381e6f9fb8c71f834e724298f0fa9c1a88a4637
brahmaredy185 <brahma@xelware.com>
2025-10-04 16:47:43 +0530

    create and test api routes

--- API/index.js
+++ API/index.js
@@ -1,11 +1,28 @@
 import express from 'express';
 import 'dotenv/config'
 import connectDB from './config/db.js';
+import userRouter from './routers/user.router.js'
 
 const app = express();
-const port = process.env.PORT || 2000
+const port = process.env.PORT || 2000;
+
+app.use(express.json())
+
+app.use('api/user',userRouter)
+app.get('/health',(req,res)=>{
+    res.status(200).json({
+        status:'ok',
+        uptime:process.uptime(),
+        timestamp:new Date().toISOString()
+    })
+})
 
 app.listen(port,()=>{
     console.log(`server running on ${port}`)
     connectDB()
 })
+
+process.on('SIGINT',()=>{
+    console.log(`server shutdown`);
+    process.exit(0);
+});
