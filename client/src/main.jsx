73a64918b7ec248a151fa0ead184bbef51df819c
brahmaredy185 <brahma@xelware.com>
2025-10-04 12:15:59 +0530

    create pages and routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './pages/Layout.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Projects from './pages/Projects.jsx'
+import Signup from './pages/Signup.jsx'
+import Signin from './pages/Signin.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+
+const routes = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout />,
+    children:[
+      {path:'/',element:<Home />},
+      {path:'/dashboard',element:<Dashboard />},
+      {path:'/projects',element:<Projects />},
+      {path:'/about',element:<About />},
+      {path:'/signin',element:<Signin />},
+      {path:'/signup',element:<Signup />},
+      {path:'*',element:<h1>404 Not Found</h1>}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
